60cc0620034996529055e021e685f0944986c6f1
devmulkalwar <devmulkalwar95@gmail.com>
2023-10-03 21:03:57 +0530

    take input and display array

--- Array/array.cpp
+++ Array/array.cpp
@@ -3,12 +3,16 @@
 using namespace std;
 int main()
 {
-    int array[10]={1,2,3,4,5,6,7,8,9,10};
+    int array[10];
 
     for (int i = 0; i < 10; i++)
     {
-        cout<<array[i]<<endl;
+        cin>>array[i];
     }
     
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<array[i]<<" ";
+    }
     return 0;
 }
