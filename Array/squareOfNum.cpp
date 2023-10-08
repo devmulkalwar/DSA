be135ff933339006fcfbf164d11abe5036769c1b
devmulkalwar <devmulkalwar95@gmail.com>
2023-10-08 23:50:32 +0530

    sqr

---
+++ Array/squareOfNum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num , square ;
+    cout<<"Enter A Number : ";
+    cin>>num;
+
+    square = num * num;
+
+    cout<<"square of number is "<< square ;
+
+    return 0;
+}
