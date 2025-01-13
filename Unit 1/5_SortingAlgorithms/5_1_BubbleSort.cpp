4dae87beef3bba7aea7f736f5be9bf6f8967f6b8
devmulkalwar <devmulkalwar95@gmail.com>
2025-01-13 12:39:07 +0530

    5.2 selection sort code added

--- Unit 1/5_SortingAlgorithms/5_1_BubbleSort.cpp
+++ Unit 1/5_SortingAlgorithms/5_1_BubbleSort.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[],int n){
-    for (int i = 0; i < n; i++)
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
     {
-        for (int j = i+1; j < n; j++){
-            if(arr[i]>arr[j]){
-            int temp = arr[j];
-            arr[j] = arr[i];
-            arr[i] = temp;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-    }   
+    }
 }
 
-void displayArray(int arr[], int n){
+void displayArray(int arr[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    
+    cout << endl;
 }
 
 int main()
@@ -28,12 +32,13 @@ int main()
     cout << "Enter the size of the array: ";
     cin >> n;
     int arr[n];
-    cout << "Enter elements : " ;
+    cout << "Enter elements: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    bubbleSort(arr,n);
-    displayArray(arr,n);
+    bubbleSort(arr, n);
+    cout << "Sorted array: ";
+    displayArray(arr, n);
     return 0;
 }
---
+++ Unit 1/5_SortingAlgorithms/5_2_SelectionSort.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j < n; j++){
+            if(arr[i]>arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = temp;
+            }
+        }
+    }   
+}
+
+void displayArray(int arr[], int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements : " ;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    selectionSort(arr,n);
+    cout<<"Sorted array is : ";
+    displayArray(arr,n);
+    return 0;
+}
