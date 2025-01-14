e1ea21241ca5dd35be82b871c057a70493e422f6
devmulkalwar <devmulkalwar95@gmail.com>
2025-01-14 16:30:16 +0530

    5.3 insertion sort code added

---
+++ Unit 1/5_SortingAlgorithms/5_3_InsertionSort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j+1] = arr[j];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+void displayArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of array : ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+    cout << "Sorted array is : ";
+    displayArray(arr, n);
+    return 0;
+}
