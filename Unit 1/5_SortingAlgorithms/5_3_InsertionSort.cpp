f1801d878a3fa67442e82a3a9c752f59f85e0427
devmulkalwar <devmulkalwar95@gmail.com>
2025-01-15 09:40:36 +0530

    minor mistake resolved

--- Unit 1/5_SortingAlgorithms/5_3_InsertionSort.cpp
+++ Unit 1/5_SortingAlgorithms/5_3_InsertionSort.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 void insertionSort(int arr[], int n)
 {
     for (int i = 1; i < n; i++)
     {
         int temp = arr[i];
-        for (int j = i - 1; j >= 0; j--)
+        int j;
+        for (j = i - 1; j >= 0 && arr[j] > temp; j--)
         {
-            if (arr[j] > temp)
-            {
-                arr[j+1] = arr[j];
-                arr[j+1] = temp;
-            }
+            arr[j + 1] = arr[j];
         }
+        arr[j + 1] = temp;
     }
 }
 
@@ -38,7 +36,7 @@ int main()
     {
         cin >> arr[i];
     }
-
+  
     insertionSort(arr, n);
     cout << "Sorted array is : ";
     displayArray(arr, n);
